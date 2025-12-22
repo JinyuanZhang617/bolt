35e5daf210f76af53ff5b5f9c446d39f76718b04
Zhang Xiaofeng <xiaofeng.zhang@bytedance.com>
2025-12-22 15:40:11 +0800

    build oss bolt with bytedance internal
    change HDFS_CLIENT_VERSION to v1.9.25.38
    
    update rebase scripts and support update bytedance_internal
    
    Temp version downgrade for gtest from 1.17.0 to 1.10.0 to be compatible with gluten
    
    sonic-cpp use internal version
    
    add comments and use commit --am for bytedance_internal change
    
    update gtest version to 1.17.0
    
    update bytedance_internal from 0ed46d2 to 081cb44
    
    Add gluten scala uts for bolt
    
    fix bolt_dwio_lance_table_scan_test failed
    
    align arrow version with oss
    
    SKIP testPaimonRowIndexWithSelectiveFilter
    
    [fix] rely on bytedance_internal JsonExtractor.cpp when building bolt_functions_json
    
    change getDcKeys signature
    
    update bytedance_internal from 06bb923 to 286de54
    
    remove internal txt
    
    update bytedance_internal from 70fb1c2 to 286de54
    
    update bytedance_internal from 286de54 to 3d9100e
    
    update bytedance_internal from 3d9100e to 363d560
    
    update bytedance_internal from 3d9100e to 363d560
    
    remove internal parquet and use sperate encruption
    
    update bytedance_internal from 8b70905 to 4e4b8d7
    
    Make SPARK_VERSION 3.2 for internal build
    
    update bytedance_internal from 4e4b8d7 to 956aa5b
    
    Disable parquet decrypt/encrypt ut in internal
    
    Disable lance by default
    
    update submodule
    
    update bytedance_internal from 765d395 to b20f703
    
    install bvc in ci.yaml
    
    update bolt_compile_img
    
    update arrow url
    
    update rapidjson
    
    Upgrade hdfs client version to v1.9.28-hotfix2
    
    update bytedance_internal from b20f703 to 17b903f
    
    update bytedance_internal from 17b903f to d8180ab
    
    update bytedance_internal from d8180ab to 932ed67
    
    update bytedance_internal from 932ed67 to 49f6077
    
    update bytedance_internal from 49f6077 to 5b2ddce
    
    Use internal version of paimon-cpp repository
    
    [CI] fix ci, it is a workaround
    
    update bytedance_internal from 3efbfe5 to 36a1383
    
    minor fix
    
    update bytedance_internal from 36a1383 to f88e3b4
    
    update bytedance_internal from f88e3b4 to ef95f3f
    
    update bytedance_internal from ef95f3f to f34739a
    
    update hdfs client to v1.9.25.38-rc.2
    
    update bytedance_internal from f34739a to b30efcc
    
    update bytedance_internal from b30efcc to 79d5dc0
    
    fix(tos): configure filesystem options
    
    update hdfs client to v1.9.25.38-rc.3
    
    update bytedance_internal from 79d5dc0 to efc100d
    
    update bytedance_internal from efc100d to 4e427d4
    
    update bytedance_internal from 4e427d4 to e1ee907

--- bolt/dwio/parquet/tests/reader/ParquetReaderTest.cpp
+++ bolt/dwio/parquet/tests/reader/ParquetReaderTest.cpp
@@ -1526,7 +1526,7 @@ TEST_F(ParquetReaderTest, arrayWithEmptyEntry) {
   assertEqualVectorPart(expected, result, 0);
 }
 
-TEST_F(ParquetReaderTest, readEncryptedParquet) {
+TEST_F(ParquetReaderTest, DISABLED_readEncryptedParquet) {
   auto rowType = ROW({"id", "name", "salary"}, {BIGINT(), VARCHAR(), BIGINT()});
 
   bytedance::bolt::dwio::common::ReaderOptions readerOpts{leafPool_.get()};
@@ -1555,7 +1555,7 @@ TEST_F(ParquetReaderTest, readEncryptedParquet) {
   EXPECT_EQ(ids->valueAt(0), 1);
 }
 
-TEST_F(ParquetReaderTest, readEncryptedParquetAllValues) {
+TEST_F(ParquetReaderTest, DISABLED_readEncryptedParquetAllValues) {
   auto rowType = ROW({"id", "name", "salary"}, {BIGINT(), VARCHAR(), BIGINT()});
 
   auto expected = makeRowVector({
@@ -1567,7 +1567,7 @@ TEST_F(ParquetReaderTest, readEncryptedParquetAllValues) {
   assertReadWithExpected("encrypted_sample.parquet", rowType, expected);
 }
 
-TEST_F(ParquetReaderTest, readEncryptedParquetWithProjection) {
+TEST_F(ParquetReaderTest, DISABLED_readEncryptedParquetWithProjection) {
   auto projectedType = ROW({"name", "salary"}, {VARCHAR(), BIGINT()});
 
   const std::string sample(getExampleFilePath("encrypted_sample.parquet"));
@@ -1593,7 +1593,7 @@ TEST_F(ParquetReaderTest, readEncryptedParquetWithProjection) {
   assertEqualVectorPart(expected, result, 0);
 }
 
-TEST_F(ParquetReaderTest, readEncryptedParquetWithFilters) {
+TEST_F(ParquetReaderTest, DISABLED_readEncryptedParquetWithFilters) {
   auto rowType = ROW({"id", "name", "salary"}, {BIGINT(), VARCHAR(), BIGINT()});
 
   FilterMap filters;
